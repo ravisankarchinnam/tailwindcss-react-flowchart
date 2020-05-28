5b767e3db97cab2d12cdc0b477336d5869375c61
ravisankarchinnam <ravisankarchinnam@gmail.com>
2020-05-28 15:32:56 +0200

    update link

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ export default function App() {
       </div>
       <a
         className="text-md underline text-gray-500 hover:text-gray-700 cursor-pointer bottom-0 absolute"
-        href="https://codesandbox.io/s/github/ravisankarchinnam/tailwindcss-flow-chart"
+        href="https://codesandbox.io/s/github/ravisankarchinnam/tailwindcss-react-flowchart"
       >
         React.js version
       </a>
